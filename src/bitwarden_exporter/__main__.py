4c2a54c4fa068258568e81ea6c214f8f5e4a404e
arpan <me@arpanrec.com>
2025-10-24 21:13:48 +0530

    wip

--- src/bitwarden_exporter/__main__.py
+++ src/bitwarden_exporter/__main__.py
@@ -141,7 +141,7 @@ def main() -> None:  # pylint: disable=too-many-locals,too-many-statements,too-m
 
     if BITWARDEN_SETTINGS.export_password.startswith("jmespath:"):
         jmespath_expression = BITWARDEN_SETTINGS.export_password[len("jmespath:") :]
-        LOGGER.info("Using JMESPath expression to extract vault password: %s", jmespath_expression)
+        LOGGER.debug("Using JMESPath expression to extract vault password: %s", jmespath_expression)
         vault_password = jmespath.search(jmespath_expression, bw_items_dict)
 
         if not vault_password:
